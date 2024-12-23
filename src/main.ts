2668f3f17b49dac2c1e5775f5fc9512720374084
Rizzz Maooo <rizqonmaulana05@gmail.com>
2024-12-23 15:31:04 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import * as cookieParser from 'cookie-parser';
+// import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +9,6 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://www.webtools.services/online-rest-api-client',
   });
-  app.use(cookieParser());
   await app.listen(configService.get('PORT'));
 }
 bootstrap();

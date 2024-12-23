86b5f95dd865cb08a0d652eabd236961f3662987
Rizqon Maulana <158800768+rizzzmaooo05@users.noreply.github.com>
2024-12-23 17:57:35 +0700

    Delete dist directory

--- dist/app.controller.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class AppController {
-    sayHello(): {
-        author: string;
-        description: string;
-    };
-}
--- dist/common/validation/validation.service.d.ts
+++
@@ -1,4 +0,0 @@
-import { ZodType } from 'zod';
-export declare class ValidationService {
-    validate<T>(zodType: ZodType<T>, data: T): T;
-}
